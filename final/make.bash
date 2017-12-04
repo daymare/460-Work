5f5fdc0d78e95530538b79fdf12c62f775e67d99
daymare <daylankelting@gmail.com>
2017-12-04 12:14:33 -0800

    added proper compile flags, fatal errors, no standard lib or includes.

--- final/make.bash
+++ final/make.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
+
 if [ $# \< 1 ]; then
    echo "make_and_copy.bash FILENAME (without .c)"
    exit
 fi
 
 arm-none-eabi-as -mcpu=arm926ej-s us.s -o us.o
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -o $1.o $1.c #2> /dev/null
+arm-none-eabi-gcc -Wfatal-errors -w -c -nostdlib -fno-builtin -nostdinc -nostartfiles -std=c99 -c -mcpu=arm926ej-s -o $1.o $1.c #2> /dev/null
 arm-none-eabi-ld -T u.ld us.o $1.o eoslib -Ttext=0x80000000 -o bin/$1
 
 mount -o loop sdimage /mnt
@@ -15,5 +16,5 @@ cp -av bin/$1 /mnt/bin
 #ls -l /mnt/bin
 umount /mnt
 
-rm $1 *.o
+rm *.o
 
